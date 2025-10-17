0ac30e31ad737fd4b87228931426e33948f25bda
CojoCosta <dcosta1882000@gmail.com>
2025-10-17 10:40:57 +0200

    changes

---
+++ Python/practica.py
@@ -0,0 +1,26 @@
+print ("Hello python", end ="")#end cambia la forma de acabar la linea
+print ("adios")
+entero = 10
+real = 12.3
+cadena = "hola"
+cadena1 = 'hola'
+booleano = True
+
+print(entero)
+print(cadena)
+print(cadena1)
+print(type(cadena))
+print(type(cadena[0]))#class str ambos son string (no existe char)
+
+print(type(entero))
+print(type(real))
+print(type(cadena))
+print(type(booleano))
+
+
+dato = int(real) + 3
+print("El numero: ", dato," es de tipo ", type(dato))
+print(float("231.33") + 3.1)
+print(str(dato) + "222")
+
+ print("no puedo poner un espacio donde me salga de los huevos")
