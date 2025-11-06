d3dd559ee735ef6a33011fbd3cd296c58675deb9
CojoCosta <dcosta1882000@gmail.com>
2025-11-06 10:11:53 +0100

    changes

--- Python/boletin/Ejercicio3/ejercicio3.py
+++ Python/boletin/Ejercicio3/ejercicio3.py
@@ -6,22 +6,33 @@ def pide_entero_positivo():
     except ValueError :
         print ("Escribe un numero")
     return numero
-# mensaje.endswith("X o un numero")
-# len(mensaje)
-# mensaje.strip() = trim()
-# mensaje.isdigit()
+
+
 def comprueba_isbn(isbn):
-    dato_sin_espacios = isbn.strip()
-    if " " in dato_sin_espacios and "-" in dato_sin_espacios:
-        dato_sin_espacios.replace(" ","")
-        dato_sin_espacios.replace("-","")
-    if len(dato_sin_espacios) == 10:
-        if dato_sin_espacios.isdigit() == False:
+    dato_sin_espacios = isbn.strip().replace(" ","").replace("-","")
+    if len(dato_sin_espacios) != 10 or not dato_sin_espacios[0:9].isdigit():
             return False
-        if (dato_sin_espacios.endswith("X") and dato_sin_espacios.count("X") == 1):
-            for caracter in range(len(dato_sin_espacios) - 1):
-                if caracter.isdigit() == False:
-                    return False
+    if (dato_sin_espacios[-1] or dato_sin_espacios.endswith("X")):
         return True
     return False
 
+def pide_libro():
+    
+
+# PROGRAMA PRINCIPAL
+isbn = "123456789X" 
+isbn2 = "1234567890"
+isbn3 = "1234X6789X"
+isbn4 = "1234567890546"
+isbn5 = "12-23 23451X"
+
+print ("1º",end="\n")
+print (comprueba_isbn(isbn),end="\n")
+print ("2º",end="\n")
+print (comprueba_isbn(isbn2),end="\n")
+print ("3º",end="\n")
+print (comprueba_isbn(isbn3),end="\n")
+print ("4º",end="\n")
+print (comprueba_isbn(isbn4),end="\n")
+print ("5º",end="\n")
+print (comprueba_isbn(isbn5),end="\n")
