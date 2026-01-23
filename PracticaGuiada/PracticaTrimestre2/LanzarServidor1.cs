0c40e8c5b84af7ad158129264098b2c1341203a7
unknown <diegocostapereira18@gmail.com>
2026-01-23 10:30:23 +0100

    changes 23/01

---
+++ PracticaGuiada/PracticaTrimestre2/LanzarServidor1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaTrimestre2
+{
+    internal class LanzarServidor1
+    {
+        public static void Main(string[] args)
+        {
+            (new Servidor1()).InitServer();
+        }
+    }
+}
